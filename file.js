c0fd0960b5574b3bfe52688f5e48afeb8a70471f
邵励治 <shaolizhi1234@126.com>
2022-11-03 11:43:13 +0800

    增加了文件读写模块

---
+++ file.js
@@ -0,0 +1,43 @@
+import fs from "fs";
+
+/**
+ * 写文件
+ * @param {*} fileName
+ * @param {*} fileContent
+ * @returns
+ */
+const writeFile = (fileName, fileContent) =>
+  new Promise((resolve, reject) => {
+    fs.writeFile(fileName, fileContent, error => {
+      if (error) {
+        console.log(`writeFile ${fileName} failure`, error);
+        reject(error);
+      } else {
+        console.log(`writeFile ${fileName} success`);
+        resolve();
+      }
+    });
+  });
+
+/**
+ * 读文件
+ * @param {*} fileName
+ * @returns
+ */
+const readFile = fileName =>
+  new Promise((resolve, reject) => {
+    fs.readFile(fileName, (error, data) => {
+      if (error) {
+        console.log(`readFile ${fileName} failure`, error);
+        reject(error);
+      } else {
+        console.log("readFile ${fileName} success", data.toString());
+        resolve(data.toString());
+      }
+    });
+  });
+
+export default {
+  writeFile,
+  readFile
+};
